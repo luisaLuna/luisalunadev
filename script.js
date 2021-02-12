8342a10f7535a3a542dbb9d53bb121e249e0a473
luisaLuna <luisacasaslu@gmail.com>
2021-02-12 16:03:10 -0500

    h1 animation

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+// Below is the code for the nav bar:
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+// Here finishes the code for the nav bar.
+
+// Below is the code for the animation on the h1 text: 
+
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'Hi, my name is Luisa Luna, and I am a front-end software developer.';
+let idx = 1
+let speed = 300 / 3
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = text.length;
+    }
+
+    setTimeout(writeText, speed)
+}
+// Here finishes the code for the animation on the h1 text.
