159a2b25be7d00c30bef8b0d3975ed4ee3cb30d0
luisaLuna <luisacasaslu@gmail.com>
2021-02-15 11:03:27 -0500

    make the navbar dissapear/appear when scrolling

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// Below is the code for the nav bar:
+// Below is the code for make the nav bar responsive:
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
+})
+
+// Below is the code to make the navbar appear and disappear when scrolling:
+const nav = document.querySelector('.navbar')
+let lastScrollTop = 0; 
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        nav.classList.add('active')  
+    } else {
+         nav.classList.remove('active') 
+     }
+      
+    lastScrollTop = scrollTop; 
 })
 // Here finishes the code for the nav bar.
 
